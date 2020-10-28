bb0ba53ced668341bb08624275d166f8839dabdc
Schwefelhexa <alexander@baron-online.eu>
2020-10-28 15:11:01 +0100

    Setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+
+import '../styles/tailwind.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default MyApp;
